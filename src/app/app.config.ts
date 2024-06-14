acf560d5ed4dc4f90e0600418c252ac173a67986
Lucas <medinalucasian@gmail.com>
2024-06-14 19:59:18 -0300

    working login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'dev-n0clp1iha3y7t6g5.us.auth0.com',
+      clientId: 'gVVNIKhgO4o3Agz7A5BftqCvSh6UrW9J',
+      authorizationParams: {
+        redirect_uri: window.location.origin + '/pickup',
+      },
+    }),
+  ],
 };
