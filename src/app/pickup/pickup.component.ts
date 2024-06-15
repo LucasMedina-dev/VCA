74049ed0e8f83891bf79a52b4f202f13c050d586
Lucas <medinalucasian@gmail.com>
2024-06-15 17:26:57 -0300

    commiting updates, still in development

--- src/app/pickup/pickup.component.ts
+++ src/app/pickup/pickup.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { VcaTitleComponent } from "../vca-title/vca-title.component";
+import { DomainCardComponent } from "../domain-card/domain-card.component";
 
 @Component({
     selector: 'app-pickup',
     standalone: true,
     templateUrl: './pickup.component.html',
     styleUrl: './pickup.component.css',
-    imports: [VcaTitleComponent]
+    imports: [VcaTitleComponent, DomainCardComponent]
 })
 export class PickupComponent{
 
