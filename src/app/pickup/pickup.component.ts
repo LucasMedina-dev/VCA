5e7cd8c9f35eab14bcec425dde6fa73015cc6946
Lucas <medinalucasian@gmail.com>
2024-06-16 01:33:15 -0300

    Still in development. Preview of domains and create new domain

--- src/app/pickup/pickup.component.ts
+++ src/app/pickup/pickup.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { VcaTitleComponent } from "../vca-title/vca-title.component";
 import { DomainCardComponent } from "../domain-card/domain-card.component";
+import { AddButtonComponent } from "../add-button/add-button.component";
 
 @Component({
     selector: 'app-pickup',
     standalone: true,
     templateUrl: './pickup.component.html',
     styleUrl: './pickup.component.css',
-    imports: [VcaTitleComponent, DomainCardComponent]
+    imports: [VcaTitleComponent, DomainCardComponent, AddButtonComponent]
 })
 export class PickupComponent{
 
