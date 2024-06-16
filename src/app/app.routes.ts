568290c5cc89450ed60a39f8837350c5a34e25d3
Lucas <medinalucasian@gmail.com>
2024-06-16 03:57:09 -0300

    Preview finished, ready for backend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './landing/landing.component';
 import { PickupComponent } from './pickup/pickup.component';
+import { CreationPageComponent } from './creation-page/creation-page.component';
 
 export const routes: Routes = [
     {path:'', component:LandingComponent},
     {path:'pickup', component:PickupComponent},
+    {path:'creation', component:CreationPageComponent},
     {path:'**', component:LandingComponent}
 ];
