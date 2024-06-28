6ebe2044abd9d49bf7d2b265130c263b393007be
Lucas <medinalucasian@gmail.com>
2024-06-28 02:35:22 -0300

    final version, ready to backend

--- src/app/pages/domain-page/add-button/add-button.component.ts
+++ src/app/pages/domain-page/add-button/add-button.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-add-button',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './add-button.component.html',
   styleUrl: './add-button.component.css'
 })
 export class AddButtonComponent {
-  constructor(private router: Router){}
 
-  redirect(){
-    this.router.navigate(['/creation'])
-  }
 }
