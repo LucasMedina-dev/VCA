c3b6a64913a8c8c036551fb2e262a640ac7c3f11
Lucas <medinalucasian@gmail.com>
2024-07-15 17:06:27 -0300

    development

--- src/app/pages/pickup/domain-card/domain-card.component.ts
+++ src/app/pages/pickup/domain-card/domain-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import DomainStruct from '../../structures/domainStruct';
+import { AlertsService } from '../../../services/alerts.service';
 
 @Component({
   selector: 'app-domain-card',
@@ -9,11 +10,18 @@ import DomainStruct from '../../structures/domainStruct';
   templateUrl: './domain-card.component.html',
   styleUrl: './domain-card.component.css',
 })
-export class DomainCardComponent{
+export class DomainCardComponent {
   @Input() domain!: any;
-  constructor(private router: Router){}
+  constructor(private router: Router, private alertsService: AlertsService) {}
   goToDomain() {
-    const domainName : String = this.domain.domainName.toLowerCase().split(" ").join("-")
-    this.router.navigate([`/stats/${domainName}`])
+    const domainName: String = this.domain.domainName
+      .toLowerCase()
+      .split(' ')
+      .join('-');
+    this.router.navigate([`/stats/${domainName}`]);
+  }
+  clipboard(stringToCopy: string, alert : string) {
+    navigator.clipboard.writeText(stringToCopy)
+    this.alertsService.showAlert(alert || "Copied!")
   }
 }
