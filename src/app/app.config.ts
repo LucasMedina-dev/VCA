3bb3f4ea50951a5509290f955ffd837c20d5c34c
Lucas <medinalucasian@gmail.com>
2024-08-24 00:24:15 -0300

    fixing redirectioning when page is reload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,7 +15,9 @@ export const appConfig: ApplicationConfig = {
       clientId: 'gVVNIKhgO4o3Agz7A5BftqCvSh6UrW9J',
       authorizationParams: {
         redirect_uri: window.location.origin
-      }
+      },
+      cacheLocation: 'localstorage',
+      skipRedirectCallback: true
     }), 
     provideAnimationsAsync(),
     provideHttpClient()
