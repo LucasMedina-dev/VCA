901a6a268e3b43013e0eb81411d10b639c992339
Lucas <medinalucasian@gmail.com>
2024-08-24 00:35:19 -0300

    logout button

--- src/app/pages/components/vca-title/vca-title.component.ts
+++ src/app/pages/components/vca-title/vca-title.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavigationButtonComponent } from "../navigation-button/navigation-button.component";
+import { AuthService } from '@auth0/auth0-angular';
+import { AlertsService } from '../../../services/alerts.service';
 
 @Component({
   selector: 'app-vca-title',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavigationButtonComponent],
   templateUrl: './vca-title.component.html',
   styleUrl: './vca-title.component.css'
 })
 export class VcaTitleComponent {
 
+  constructor(private auth : AuthService, private alert: AlertsService){}
+
+  logOut() {
+    this.auth.logout().subscribe({
+      next:(data)=>{
+        this.alert.showAlert("Logged out!")
+      }
+    })
+  }
 }
